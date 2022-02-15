6892367c9ff1b7859de68825ffccb1dc269e92ca
Michel3-01 <95318940+Michel3-01@users.noreply.github.com>
2022-02-14 22:01:48 -0200

    Alterações

--- controller/relatorio_page.py
+++ controller/relatorio_page.py
@@ -9,6 +9,10 @@ class RelatorioPage(QWidget):
         super().__init__()
         uic.loadUi(FILE_UI, self)
 
+
+        #Função do botão calcular.
+        self.calcular_btn.clicked.connect(self.calcular_lucro_prejuizo)
+
         self.carrega_vendas()
     def carrega_vendas(self):
 
@@ -19,11 +23,11 @@ class RelatorioPage(QWidget):
         for vendas in lista:
             self.add_linha(vendas)
             lista_valores_venda.append(vendas.valor)
-        valor_total_vendas = 0
+        self.valor_total_vendas = 0
         for x in lista_valores_venda:
-                valor_total_vendas += x
+                self.valor_total_vendas += x
                
-        self.label_valor_total.setText(str(valor_total_vendas))
+        self.label_valor_total.setText(str(self.valor_total_vendas))
         self.label_quantidade.setText(str(len(lista_valores_venda)))
 
     def add_linha(self, vendas):
@@ -41,6 +45,20 @@ class RelatorioPage(QWidget):
         self.tabela_relatorio.setItem(rowCount,1,nome)
         self.tabela_relatorio.setItem(rowCount,2,email)
         self.tabela_relatorio.setItem(rowCount,3,valor)
+    
+    def calcular_lucro_prejuizo(self):
+        despesas = int(self.label_despesas.text())
+        resultado = self.valor_total_vendas - despesas
+        if resultado > 0:
+            self.label_lucro_prejuizo.setText(f"A empresa teve lucro de R$:{resultado}")
+            self.label_lucro_prejuizo.setStyleSheet("color: green; background-color: black; font-size: 35px")
+        elif resultado == 0:
+            self.label_lucro_prejuizo.setText(f"A empresa não teve lucros nem prejuizo de R$:{resultado}")
+            self.label_lucro_prejuizo.setStyleSheet("color: white; background-color: black; font-size: 35px")
+        else:
+            self.label_lucro_prejuizo.setText(f"A empresa teve prejuizo de R$:{resultado}")
+            self.label_lucro_prejuizo.setStyleSheet("color: red; background-color: black; font-size: 35px")
+            
          
       
             
